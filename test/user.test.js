0d73f33bd957522d35a386ba8bb10979acf85ded
Tahmid Tanzim <tahmid.tanzim@gmail.com>
2021-07-31 16:53:10 +0600

    Create user API testing

--- test/user.test.js
+++ test/user.test.js
@@ -5,10 +5,38 @@ import { expect } from 'chai';
 const request = supertest(server);
 
 describe("API Test for User", () => {
-    it("return all users", async () => {
-        const response = await request.get("/airports");
+
+    before(() => {
+        console.log('1');
+    });
+
+    after(() => {
+        console.log('4');
+    });
+
+    beforeEach(() => {
+        console.log('2');
+    });
+
+    afterEach(() => {
+        console.log('3');
+    });
+
+    it("Check invalid routes /error", async () => {
+        const response = await request.get("/error");
 
         expect(response.status).to.eql(404);
         expect(response.body.message).to.eql("404 Not Found");
+    });
+
+    it("Create new user", async () => {
+        const response = await request
+            .post('/v1/users')
+            .send({ first_name: 'Tahmid', last_name: 'Tanzim', email: 'tahmid.tanzim@gmail.com', date_of_birth: '1971-12-16' })
+            .set('Accept', 'application/json');
+
+        expect('Content-Type', /json/);
+        expect(response.status).to.eq(201);
+        expect(response.body.data.first_name).to.eql("Tahmid");
     });
 });
